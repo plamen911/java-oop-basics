384e127ebbeda1567bfcb0894d80c9f77061b0e8
apple <1>
2016-12-26 19:15:25 +0200

    Create Rectangle Intersection

---
+++ src/org/lynxlake/methods/rectangleIntersection/Rectangle.java
@@ -0,0 +1,41 @@
+package org.lynxlake.methods.rectangleIntersection;
+
+class Rectangle {
+    private String id;
+    private int width;
+    private int height;
+    private int topLeftX;
+    private int topLeftY;
+
+    public Rectangle(String id, int width, int height, int topLeftX, int topLeftY) {
+        this.id = id;
+        this.width = width;
+        this.height = height;
+        this.topLeftX = topLeftX;
+        this.topLeftY = topLeftY;
+    }
+
+    public boolean hasIntersection(Rectangle r) {
+        return this.topLeftX < r.getTopLeftX() + r.width && this.topLeftX + width > r.getTopLeftX() && this.topLeftY < r.getTopLeftY() + r.height && this.topLeftY + height > r.getTopLeftY();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getTopLeftX() {
+        return topLeftX;
+    }
+
+    public int getTopLeftY() {
+        return topLeftY;
+    }
+}

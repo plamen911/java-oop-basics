5f7f32cf07db712fa607f3a4853c1e81e5d4f686
apple <1>
2016-12-26 23:24:30 +0200

    Create Planck Constant

---
+++ src/org/lynxlake/staticMembers/planckConstant/Main.java
@@ -0,0 +1,8 @@
+// https://judge.softuni.bg/Contests/Practice/Index/231#5
+package org.lynxlake.staticMembers.planckConstant;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Calculation.calculate());
+    }
+}

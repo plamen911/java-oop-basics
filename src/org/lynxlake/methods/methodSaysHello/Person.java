1019721c49823703bd5f4aa36bf7e8a3f2a96c05
apple <1>
2016-12-26 11:18:16 +0200

    Create Last Digit Name

--- src/org/lynxlake/methods/methodSaysHello/Person.java
+++ src/org/lynxlake/methods/methodSaysHello/Person.java
@@ -4,6 +4,7 @@ class Person {
     private String name;
 
     public String sayHello(String name) {
-        return String.format("%s says Hello!", name);
+        this.name = name;
+        return String.format("%s says \"Hello\"!", this.name);
     }
 }

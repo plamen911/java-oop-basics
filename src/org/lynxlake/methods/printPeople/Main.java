08cc5ab71c3d3b4e3892c34df7d50ac6ac12fbeb
apple <1>
2016-12-26 19:40:34 +0200

    Create Print People

---
+++ src/org/lynxlake/methods/printPeople/Main.java
@@ -0,0 +1,38 @@
+// https://judge.softuni.bg/Contests/Practice/Index/225#12
+package org.lynxlake.methods.printPeople;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Person> people = new ArrayList<>();
+
+        String line = reader.readLine().trim();
+        while (!line.equalsIgnoreCase("END")) {
+            String[] params = line.split("\\s+");
+            String name = params[0];
+            int age = Integer.parseInt(params[1]);
+            String occupation = params[2];
+
+            Person person = new Person(name, age, occupation);
+            people.add(person);
+
+            line = reader.readLine().trim();
+        }
+
+        Collections.sort(people);
+
+        for (int i = 0; i < people.size(); i++) {
+            System.out.println(people.get(i));
+        }
+
+        reader.close();
+    }
+}
---
+++ src/org/lynxlake/methods/printPeople/Person.java
@@ -0,0 +1,27 @@
+package org.lynxlake.methods.printPeople;
+
+class Person implements Comparable<Person> {
+    private String name;
+    private int age;
+    private String occupation;
+
+    public Person(String name, int age, String occupation) {
+        this.name = name;
+        this.age = age;
+        this.occupation = occupation;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s - age: %d, occupation: %s", this.name, this.age, this.occupation);
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return Integer.compare(this.getAge(), o.getAge());
+    }
+}

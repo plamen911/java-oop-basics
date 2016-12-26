f1ca1008fb8d1da74f8fdd5982981dba72726810
apple <1>
2016-12-26 10:28:59 +0200

    Create Google and Family Tree

---
+++ src/org/lynxlake/definingClasses/google/Person.java
@@ -0,0 +1,68 @@
+package org.lynxlake.definingClasses.google;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Person {
+    private String name;    // unique
+    private Company company;
+    private Car car;
+    private List<Pokemon> pokemons;
+    private List<Parent> parents;
+    private List<Child> children;
+
+    Person(String name) {
+        this.name = name;
+        this.pokemons = new ArrayList<>();
+        this.parents = new ArrayList<>();
+        this.children = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public List<Pokemon> getPokemons() {
+        return pokemons;
+    }
+
+    public void addPokemon(Pokemon pokemon) {
+        this.pokemons.add(pokemon);
+    }
+
+    public List<Parent> getParents() {
+        return parents;
+    }
+
+    public void addParent(Parent parent) {
+        this.parents.add(parent);
+    }
+
+    public List<Child> getChildren() {
+        return children;
+    }
+
+    public void addChild(Child child) {
+        this.children.add(child);
+    }
+}

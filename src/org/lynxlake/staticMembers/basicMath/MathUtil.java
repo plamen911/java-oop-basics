d9133e183502b1c9bba8eec1bd9cf875679ac6fe
apple <1>
2016-12-26 23:45:01 +0200

    Create Basic Math

---
+++ src/org/lynxlake/staticMembers/basicMath/MathUtil.java
@@ -0,0 +1,23 @@
+package org.lynxlake.staticMembers.basicMath;
+
+class MathUtil {
+    static double sum(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    static double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    static double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    static double divide(double num1, double num2) {
+        return num1 / num2;
+    }
+
+    static double percentage(double num1, double num2) {
+        return num1 * num2 / 100;
+    }
+}

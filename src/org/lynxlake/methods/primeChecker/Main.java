53395587aee3ea0fd6580e7142faa9df61d95e61
apple <1>
2016-12-26 16:18:10 +0200

    Create Prime Checker

---
+++ src/org/lynxlake/methods/primeChecker/Main.java
@@ -0,0 +1,39 @@
+// https://judge.softuni.bg/Contests/Practice/Index/225#6
+package org.lynxlake.methods.primeChecker;
+
+import com.sun.org.apache.bcel.internal.classfile.ClassFormatException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        long n = Integer.parseInt(reader.readLine());
+
+        reader.close();
+
+        Number number = new Number(n);
+        System.out.printf("%s", number.getNextPrime());
+
+        Field[] fields = Number.class.getDeclaredFields();
+
+        List<Field> filedsDeclared = Arrays.stream(fields)
+                .filter(f -> f.getName().contains("prime") || f.getName().contains("number"))
+                .collect(Collectors.toList());
+
+        List<Constructor<?>> constructors = Arrays.stream(Number.class.getDeclaredConstructors())
+                .filter(c -> c.getParameterCount() > 1)
+                .collect(Collectors.toList());
+
+        if (filedsDeclared.size() <= 1 || constructors.size() < 1) {
+            throw new ClassFormatException();
+        }
+    }
+}

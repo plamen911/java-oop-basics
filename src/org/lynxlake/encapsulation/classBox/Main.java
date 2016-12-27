c51fa82d46ba1c5e7b6174da79ba4e498c55bfee
apple <1>
2016-12-27 09:38:34 +0200

    Create Class Box

---
+++ src/org/lynxlake/encapsulation/classBox/Main.java
@@ -0,0 +1,30 @@
+// https://judge.softuni.bg/Contests/Practice/Index/221#0
+package org.lynxlake.encapsulation.classBox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        double length = Double.parseDouble(reader.readLine());
+        double width = Double.parseDouble(reader.readLine());
+        double height = Double.parseDouble(reader.readLine());
+        reader.close();
+
+        Class boxClass = Box.class;
+        Field[] fields = boxClass.getDeclaredFields();
+        System.out.println(Arrays.asList(fields)
+                .stream()
+                .filter(f -> Modifier.isPrivate(f.getModifiers())).count());
+
+        Box box = new Box(length, width, height);
+        System.out.printf("Surface Area - %.2f%n", box.getSurfaceArea());
+        System.out.printf("Lateral Surface Area - %.2f%n", box.getLateralSurfaceArea());
+        System.out.printf("Volume - %.2f%n", box.getVolume());
+    }
+}
